da42d4247bf31ce30764d466a6a236b98e1d553e
rhizodigital <rhizodigital@gmail.com>
2021-11-21 18:04:24 +0000

    Add demo HTML,CSS. Add production settings

--- planetwide/settings/production.py
+++ planetwide/settings/production.py
@@ -2,6 +2,16 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'name',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 try:
     from .local import *
 except ImportError:
