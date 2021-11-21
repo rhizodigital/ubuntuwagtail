a4b44a3f31b0f8020d966ba333db763a24467626
rhizodigital <rhizodigital@gmail.com>
2021-11-21 17:49:13 +0000

    Initial commit

---
+++ planetwide/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-)+rhg-!b&k^lw2c^7*q_tx+wjvs17()((vw_ivbli4*^3dvu&j'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
